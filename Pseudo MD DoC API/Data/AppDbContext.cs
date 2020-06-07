709d4b43926a3f77b891349ec7789811975ff145
Ricky Theil <rtheil@codirt.com>
2020-06-06 23:20:11 -0600

    Rename DB Context

--- Pseudo MD DoC API/Data/AppDbContext.cs
+++ Pseudo MD DoC API/Data/AppDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Pseudo_MD_DoC_API.Applications;
+using Pseudo_MD_DoC_API.Users;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,12 @@ namespace Pseudo_MD_DoC_API.Persistence
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<Application> Applications { get; set; }
+        public DbSet<User> Users { get; set; }
+
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-
         }
+
     }
 }
