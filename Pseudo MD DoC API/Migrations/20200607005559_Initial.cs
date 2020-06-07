99e52075e211766c0ac5bf77c0b868173f36df49
Ricky Theil <rtheil@codirt.com>
2020-06-06 23:20:54 -0600

    Database migrations

---
+++ Pseudo MD DoC API/Migrations/20200607005559_Initial.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pseudo_MD_DoC_API.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Applications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(maxLength: 25, nullable: false),
+                    LastName = table.Column<string>(maxLength: 25, nullable: false),
+                    MiddleInitial = table.Column<string>(maxLength: 1, nullable: false),
+                    Address = table.Column<string>(maxLength: 100, nullable: false),
+                    City = table.Column<string>(maxLength: 30, nullable: false),
+                    State = table.Column<string>(maxLength: 2, nullable: false),
+                    ZipCode = table.Column<string>(maxLength: 5, nullable: false),
+                    HomePhone = table.Column<string>(maxLength: 10, nullable: false),
+                    CellPhone = table.Column<string>(maxLength: 10, nullable: false),
+                    EMailAddress = table.Column<string>(maxLength: 255, nullable: false),
+                    SocialSecurityNumber = table.Column<string>(maxLength: 11, nullable: false),
+                    IsUsCitizen = table.Column<bool>(nullable: false),
+                    HasFelony = table.Column<bool>(nullable: false),
+                    WillDrugTest = table.Column<bool>(nullable: false),
+                    ApplicationStatus = table.Column<int>(nullable: false),
+                    TestScore = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Applications", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Education",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SchoolName = table.Column<string>(maxLength: 100, nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: true),
+                    Degree = table.Column<string>(maxLength: 50, nullable: false),
+                    ApplicationId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Education", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Education_Applications_ApplicationId",
+                        column: x => x.ApplicationId,
+                        principalTable: "Applications",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployerName = table.Column<string>(nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: true),
+                    phone = table.Column<string>(nullable: false),
+                    Position = table.Column<string>(nullable: false),
+                    CanContact = table.Column<bool>(nullable: false),
+                    ApplicationId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employment_Applications_ApplicationId",
+                        column: x => x.ApplicationId,
+                        principalTable: "Applications",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reference",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    PhoneNumber = table.Column<string>(nullable: false),
+                    Relation = table.Column<string>(nullable: false),
+                    ApplicationId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reference", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reference_Applications_ApplicationId",
+                        column: x => x.ApplicationId,
+                        principalTable: "Applications",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Education_ApplicationId",
+                table: "Education",
+                column: "ApplicationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employment_ApplicationId",
+                table: "Employment",
+                column: "ApplicationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reference_ApplicationId",
+                table: "Reference",
+                column: "ApplicationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Education");
+
+            migrationBuilder.DropTable(
+                name: "Employment");
+
+            migrationBuilder.DropTable(
+                name: "Reference");
+
+            migrationBuilder.DropTable(
+                name: "Applications");
+        }
+    }
+}
---
+++ Pseudo MD DoC API/Migrations/20200607041741_RemoveNullable.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pseudo_MD_DoC_API.Migrations
+{
+    public partial class RemoveNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Relation",
+                table: "Reference",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CellPhone",
+                table: "Applications",
+                maxLength: 10,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Relation",
+                table: "Reference",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CellPhone",
+                table: "Applications",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 10,
+                oldNullable: true);
+        }
+    }
+}
