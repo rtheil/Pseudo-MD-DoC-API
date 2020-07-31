0f9c4d6e2a1800c29c88755c5f4b3869d80efe79
Ricky Theil <rtheil@codirt.com>
2020-07-30 21:05:22 -0600

    Removed internal publish profile

--- Pseudo MD DoC API/Queries/Users.sql
+++ Pseudo MD DoC API/Queries/Users.sql
@@ -4,8 +4,3 @@ update users set accountVerified=1
 
 select * from configuration
 
-insert into configuration (propertyName,propertyValue) values ('newUserFromAddress','rtheil@codirt.com')
-insert into configuration (propertyName,propertyValue) values ('newUserFromName','md-doc-web')
-insert into configuration (propertyName,propertyValue) values ('newUserSubject','Please verify your account')
-insert into configuration (propertyName,propertyValue) values ('newUserUrl','http://localhost:3000/login/register')
-insert into configuration (propertyName,propertyValue) values ('newUserContent','Click this link to verify your account: [newUserUrl]')
